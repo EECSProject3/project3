c94cb7730a036ef889990a6926a4384158541559
maryvogler <60023461+maryvogler@users.noreply.github.com>
2020-02-24 15:36:15 -0500

    Add files via upload

--- polybius.cpp
+++ polybius.cpp
@@ -2,8 +2,8 @@
  * polybius.cpp
  * Project UID e98fd45ccce9d7195e89e6171a5451f2
  *
- * Shayla Coombs, Mary Volger
- * sjcoombs,
+ * <#Names#>
+ * <#Uniqnames#>
  *
  * EECS 183: Project 3
  * Winter 2020
@@ -13,28 +13,47 @@
 
 
 #include "polybius.h"
+#include "utility.h"
 #include <string>
+#include <iostream>
+
 using namespace std;
 
 
 void fillGrid(char grid[SIZE][SIZE], string content) {
-    for(int i = 0;  i  < content.length();  i++) {
-        printGrid(grid);
+    int contentCount = 0;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            grid[i][j] = content.at(contentCount);
+            contentCount++;
+        }
     }
 }
 
 
+
 string mixKey(string key) {
-    string removing = key  + ALNUM;
-    return removeDuplicate(removing);
-}
+      string removing = key  + ALNUM;
+      return removeDuplicate(removing);
+  }
+
 
 
 string findInGrid(char c, char grid[SIZE][SIZE]) {
-    // TODO: implement
-
-    // returning a string to avoid compile error
-    return "";
+    string newString = "";
+    int row;
+    int col;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (grid[i][j] == c) {
+                row = i + '0';
+                col = j + '0';
+                newString += row;
+                newString += col;
+            }
+        }
+    }
+    return newString;
 }
 
 string polybiusSquare(char grid[SIZE][SIZE], string key, string original, bool encrypt) {
