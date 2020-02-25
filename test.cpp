7deb630d9fba980a3160a9bbbebec5012046e337
sjcoombs <59891852+sjcoombs@users.noreply.github.com>
2020-02-25 10:34:40 -0500

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -25,6 +25,11 @@ void testCaesarCipher();
 void testRemoveNonAlphas();
 void testRemoveDuplicate();
 void testCharToInt();
+void testVigenereCipher();
+void testFillGrid();
+void testMixKey();
+void testFindInGrid();
+void testPolybiusSquare();
 
 void startTests() {
     testShiftAlphaCharacter();
@@ -33,7 +38,11 @@ void startTests() {
     testRemoveNonAlphas();
     testRemoveDuplicate();
     testCharToInt();
-    // Repeat for all other functions to be tested
+    testVigenereCipher();
+    testFillGrid();
+    testMixKey();
+    testFindInGrid();
+    testPolybiusSquare();
 
     return;
 }
@@ -48,18 +57,19 @@ void testShiftAlphaCharacter() {
 
 void testToUpperCase() {
     cout << "Now testing function toUpperCase()" << endl;
-    cout << "Expected: HAPPY, Actual: '" << toUpperCase("happy") << "'" << endl;
-    cout << "Expected: ROG2ER, Actual: '" << toUpperCase("rog2er") << "'" << endl;
-    cout << "Expected: B, Actual: '" << toUpperCase("b") << "'" << endl;
+    cout << "Expected: HAPPY, Actual: " << toUpperCase("happy") << endl;
+    cout << "Expected: ROG2ER, Actual: " << toUpperCase("rog2er") << endl;
+    cout << "Expected: B, Actual: " << toUpperCase("b") << endl;
     return;
     
 }
 
 void testRemoveNonAlphas() {
     cout << "Now testing function removeNonAlphas()" << endl;
-    cout << "Expected: happy, Actual: '" << removeNonAlphas("happy") << "'" << endl;
-    cout << "Expected: roger, Actual: '" << removeNonAlphas("rog2er") << "'" << endl;
-    cout << "Expected: b, Actual: '" << removeNonAlphas("b8$") << "'" << endl;
+    cout << "Expected: happy, Actual: " << removeNonAlphas("happy") << endl;
+    cout << "Expected: roger, Actual: " << removeNonAlphas("rog2er") << endl;
+    cout << "Expected: b, Actual: " << removeNonAlphas("b8$") << endl;
+    cout << "Expected: Hellothere, Actual: " << removeNonAlphas("Hello there.") << endl;
     return;
     
 }
@@ -84,35 +94,58 @@ void testCharToInt() {
 
 void testCaesarCipher() {
     cout << "Now testing function caesarCipher()" << endl;
-    cout << "Now testing function toUpperCase()" << endl;
-    cout << "Expected: , Actual: " << caesarCipher("Hello", 5, false) << "" << endl;
-    cout << "Expected: , Actual: " << caesarCipher("Hello", 5, true) << "" << endl;
-    cout << "Expected: , Actual: " << caesarCipher("friendLY", 4, true) << "" << endl;
+    cout << "Expected: Czggj, Actual: " << caesarCipher("Hello", 5, false) << "" << endl;
+    cout << "Expected: Mjqqt, Actual: " << caesarCipher("Hello", 5, true) << "" << endl;
+    cout << "Expected: jvmirhPC, Actual: " << caesarCipher("friendLY", 4, true) << "" << endl;
+    cout << "Expected: JVMIRHW, Actual: " << caesarCipher("FRIENDS", 4, true) << "" << endl;
+    cout << "Expected: BNEAJZO, Actual: " << caesarCipher("FRIENDS", 4, false) << "" << endl;
     return;
     
 }
 
 void testVigenereCipher() {
     cout << "Now testing function vigenereCipher()" << endl;
+    cout << "Expected: OLSSV, Actual: " << vigenereCipher("hello", "z" , false) << "" << endl;
+    cout << "Expected: sdwp eo ql pdeo eo odwuhw, Actual: " << vigenereCipher("what is up this is shayla", "w", true) << "" << endl;
+    cout << "Expected: Kcmf nspbk kk bgbq, Actual: " << vigenereCipher("Yeah buddy my dude", "my", true) << "" << endl;
+    return;
     
 }
 
 void testFillGrid() {
     cout << "Now testing function fillGrid()" << endl;
+    char grid[SIZE][SIZE];
+    fillGrid(grid, ALNUM);
+    printGrid(grid);
+    fillGrid(grid, "QWERTUIOPA SDFGHJKLZXCVBNM4356789021");
     
 }
 
 void testMixKey() {
     cout << "Now testing function mixKey()" << endl;
+    cout << "Expected: HAPYBCDEFGIJKLMNOQRSTUVWXYZ0123456789, Actual: " << mixKey("HAPY") << "" << endl;
+    cout << "Expected: SUP98ABCDEFGHIJKLMNOQURTVWXYZ01234567, Actual: " << mixKey("SUP98") << "" << endl;
+    cout << "Expected: FRIENDSABCGHJKLMOPQTUVWXYZ0123456789, Actual: " << mixKey("FRIENDS") << "" << endl;
+    return;
+    
     
 }
 
 void testFindInGrid() {
     cout << "Now testing function findInGrid()" << endl;
+    char grid[SIZE][SIZE];
+    fillGrid(grid, ALNUM);
+    cout << findInGrid('A', grid) << endl;
+    cout << findInGrid('9', grid) << endl;
+    cout << findInGrid('Z', grid) << endl;
+    cout << findInGrid('0', grid) << endl;
+    cout << findInGrid('F', grid) << endl;
     
 }
 
 void testPolybiusSquare() {
     cout << "Now testing function polybiusSquare()" << endl;
-    
+    char grid[SIZE][SIZE];
+    cout << polybiusSquare(grid, "183", "EECS 183 IS THE BEST", true) << endl;
+    cout << polybiusSquare(grid, "HAY", "SHAYLA", false) << endl;
 }
